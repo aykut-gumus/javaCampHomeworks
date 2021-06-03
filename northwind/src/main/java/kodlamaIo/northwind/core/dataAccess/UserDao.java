b2086d158215f641dfd8498b12434b57f147aa3d
Aykut Gümüş <79155927+AykutGm@users.noreply.github.com>
2021-06-03 13:49:42 +0300

    Delete northwind directory

--- northwind/src/main/java/kodlamaIo/northwind/core/dataAccess/UserDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaIo.northwind.core.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaIo.northwind.core.entities.User;
-
-public interface UserDao extends JpaRepository<User, Integer>{
-	User findByEmail(String email);
-}
